2cf08fbe975f32b0e5f0a1d658af6a89f829ed3d
Ciera <177058032+QueenCee48@users.noreply.github.com>
2024-12-08 10:57:39 -0500

    Create contact.js

---
+++ ContactPage/contact.js
@@ -0,0 +1,59 @@
+let mouseCursor = document.querySelector(".cursor");
+
+window.addEventListener('mousemove',cursor);
+
+function cursor(event) {
+  mouseCursor.style.top = event.pageY + "px";
+  mouseCursor.style.left = event.pageX + "px";
+}
+
+const form = document.getElementById("contactForm");
+const responseMessage = document.getElementById("responseMessage");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const urlEncodedData = new URLSearchParams();
+  
+  formData.forEach((value, key) => {
+    urlEncodedData.append(key, value);
+  });
+  
+  fetch(form.action, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: urlEncodedData
+  })
+  
+  .then(response => {
+    if (response.status === 200) {
+      form.reset();
+      responseMessage.style.display = "block";
+    }
+  })
+  
+  .catch(error => {
+    console.error('Error:', error);
+    
+    $('.alert').removeClass("hide");
+    $('.alert').addClass("show");
+    $('.alert').addClass("showAlert");
+    
+    setTimeout(function(){
+      $('.alert').addClass("hide");
+      $('.alert').removeClass("show");
+      $('.alert').removeClass("showAlert");
+    },5000);
+    
+    $('.close-btn').click(function(){
+      $('.alert').addClass("hide");
+      $('.alert').removeClass("show");
+      $('.alert').removeClass("showAlert");
+    });
+    
+    form.reset();
+  });
+  
+});
